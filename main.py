e51e79fcdd28e53db5e8ef9196ab0831f62c312a
Joel David <jdavidhc1710@gmail.com>
2018-11-09 14:28:10 -0500

    Finally integrated CodeFlask and html input field

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 import forms
 
@@ -12,9 +13,14 @@ def index():
     return render_template("index.html", title=title)
 
 
-@app.route("/new_widget")
+@app.route("/new_widget", methods=['GET', 'POST'])
 def new_widget():
-    widget = forms.Widget()
+    widget = forms.Widget(request.form)
+
+    if request.method == 'POST':
+        print(widget.name.data)
+        print(widget.html.data)
+
     title = "New Widget"
     return render_template("widget.html", title=title, form=widget)
 
