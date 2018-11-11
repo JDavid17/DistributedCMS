8b09c0835c018e30c924c89778a1ddb27ed49b79
Joel David <jdavidhc1710@gmail.com>
2018-11-10 20:38:26 -0500

    Added PyMongo

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from bs4 import BeautifulSoup
+from flask_pymongo import PyMongo
 
 import forms
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/CMS"
+mongo = PyMongo(app)
 
 
 @app.route("/")
@@ -18,8 +22,8 @@ def new_widget():
     widget = forms.Widget(request.form)
 
     if request.method == 'POST' and widget.validate():
-        print(widget.name.data)
-        print(widget.html.data)
+        
+        pass
 
     title = "New Widget"
     return render_template("widget.html", title=title, form=widget)
