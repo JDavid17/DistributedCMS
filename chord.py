026efc27331041a437dd384c892d8ec55a68d92a
Hieu Do Ngoc <hieudn.uh@gmail.com>
2018-12-04 11:44:28 -0500

    Minor code improvements to chord.py

--- chord.py
+++ chord.py
@@ -2,7 +2,7 @@ from ckey import *
 from settings import *
 from network import *
 from log import *
-import Pyro4
+import Pyro4 
 import threading
 
 
@@ -12,19 +12,18 @@ class ChordNode:
 
         self.next = -1
         self.finger = [None for i in range(M)]
-        # TODO: Implement a separate class for chord id
+        #TODO: Implement a separate class for chord id
         self._key = ChordKey(ip, port)
         self._successors = [None]
         self._predecessor = None
         self._running = False
 
+
     @property
-    @Pyro4.expose
     def key(self):
         return self._key
 
     @property
-    @Pyro4.expose
     def id(self):
         return self._key.id
 
@@ -47,6 +46,8 @@ class ChordNode:
     @property
     def running(self):
         return self._running
+
+
 
     def join(self, ip=None, port=None):
         if not ip or not port:
@@ -88,21 +89,21 @@ class ChordNode:
         currentNode = self.key
         while True:
             with remote(currentNode) as n:
-                if betweenclosedopen(key, n.id, n.successor().id):
+                if betweenclosedopen(key, n.id, n.successor().id): 
                     break
                 currentNode = n.closest_preceding_node(key)
         return currentNode
 
     def closest_preceding_node(self, key):
-        for i in range(len(self.finger) - 1, 0, -1):
+        for i in range(len(self.finger)-1, 0, -1):
             if self.finger[i] and betweenclosedclosed(self.finger[i].id, self.id, key):
                 return self.finger[i]
         return self.successor() if betweenclosedclosed(self.successor().id, self.id, key) else self.key
 
     def notify(self, predKey):
-        if self.predecessor is None or betweenclosedclosed(predKey.id, self.predecessor.id, self.id) or not ping(
-                self.predecessor):
+        if self.predecessor == None or betweenclosedclosed(predKey.id, self.predecessor.id, self.id) or not ping(self.predecessor):
             self.predecessor = predKey
+
 
     @repeat_wait(STABILIZE_WAIT)
     def stabilize(self):
@@ -115,14 +116,14 @@ class ChordNode:
 
         with remote(suc) as successorNode:
             x = successorNode.predecessor
-            if self._successors[0].id == successorNode.id and betweenclosedclosed(x.id, self.id, successorNode.id):
+            if self._successors[0].id == successorNode.id and betweenclosedclosed(x.id, self.id, successorNode.id): 
                 newsuclist = [x]
             with remote(newsuclist[0]) as newSuccessor:
                 newSuccessor.notify(self.key)
 
         # log("updating successors list")
         if self.id != self.successor().id:
-            newsuclist += remote(newsuclist[0]).successors[:N_SUCCESSORS - 1]
+            newsuclist += remote(newsuclist[0]).successors[:N_SUCCESSORS-1]
             self._successors = newsuclist
         # log("successorlist: ", self.successors, "\n")
 
@@ -131,7 +132,8 @@ class ChordNode:
         self.next = self.next + 1
         if self.next >= M:
             self.next = 0
-        self.finger[self.next] = self.find_successor(intToKey(sumHexInt(self.id, 2 ** self.next) % 2 ** M, M))
+        self.finger[self.next] = self.find_successor(intToKey(sumHexInt(self.id, 2**self.next) % 2**M, M))
+
 
     def start(self):
         if not self.running:
@@ -147,7 +149,7 @@ class ChordNode:
         pass
 
 
-def start_new_node(node, ip, port):
+def start_new_node(node, ip, port):    
     with Pyro4.Daemon(host=host, port=int(port)) as daemon:
         uri = daemon.register(node, objectId=node.id)
         log(str(uri))
@@ -158,10 +160,8 @@ def start_new_node(node, ip, port):
 if __name__ == "__main__":
     def converter(classname, dict):
         return ChordKey(dict['ip'], dict['port'])
-
-
     Pyro4.util.SerializerBase.register_dict_to_class("ckey.ChordKey", converter)
-
+    
     # host = input("IP: ")
     host = "localhost"
     port = input("Port: ")
@@ -179,3 +179,11 @@ if __name__ == "__main__":
         l = cmd.split()
         if l[0] == "join":
             node.join("localhost", l[1])
+
+
+# # TESTS
+# 10000 #2
+# 11028 #7
+# 11528 #8
+# 11021 #e
+# 22220 #0
