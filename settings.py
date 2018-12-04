d17386cf8336ba08e9d90dfeb1ef20489f2c7553
Hieu Do Ngoc <hieudn.uh@gmail.com>
2018-12-04 11:49:10 -0500

    Code cleanup and add test.py to gitignore

--- settings.py
+++ settings.py
@@ -1,11 +1,14 @@
-M = 4  # 2^M size of Chord ring
 
-N_SUCCESSORS = 3  # Number of successors to keep track of
+M = 4                   # 2^M size of Chord ring
 
-CON_RETRIES = 3  # Number of times to attemp a reconnection to a peer
+N_SUCCESSORS = 3        # Number of successors to keep track of
 
-STABILIZE_WAIT = .5  # Time to wait between each stabilization call (seconds)
 
-FFINGERS_WAIT = .5  # Time to wait between each fix fingers call (seconds)
+CON_RETRIES = 3         # Number of times to attemp a reconnection to a peer
 
-DISTRIBUTE_WAIT = 1  # Time to wait between each data distribution routine (seconds)
+STABILIZE_WAIT = .5     # Time to wait between each stabilization call (seconds)
+
+FFINGERS_WAIT = .5      # Time to wait between each fix fingers call (seconds)
+
+
+DISTRIBUTE_WAIT = 1     # Time to wait between each data distribution routine (seconds)
